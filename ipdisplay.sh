3ecfdd593d2d9980dd5dae63ff827e15f6e896b2
xiangvliu <xiang.v.liu@gmail.com>
2018-01-13 23:29:08 +0800

    Init oled code

---
+++ ipdisplay.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+host_name=`hostname`
+
+#interface_name=`cat /proc/net/dev|tail -n +3|grep -v lo|awk -F: '{print $1}'`
+
+while true;
+do
+#  ipaddress=`ifconfig $interface_name | grep -w "inet" | awk '{ print $2}'`
+  ipaddress=`hostname -I`
+  if [ "$ipaddress" ];then
+    break
+  else
+    sleep 3
+    continue
+  fi
+done
+
+/home/pi/oledi2c -f 2 -s -l 30 "Hostname: "$host_name" IP Address: "$ipaddress &
